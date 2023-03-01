be3fd239657af7b2bbddbbaf30552c104161b62a
dhbroad <dhbroad@udel.edu>
2023-02-28 21:02:39 -0500

    changed view links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,17 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="#">Navbar</Link>
+    <Link className="navbar-brand" to="/">Random Pokemon Generator</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          <Link className="nav-link active" aria-current="page" to="/about">About</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="#">Link 1</Link>
+          <Link className="nav-link" to="/contact">Contact</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="#">Link 2</Link>
